12a204197f68fd2a679f37511b0fd1236826ceea
Aliza Dixit <alizadixit6@gmail.com>
2023-10-11 01:39:35 +0530

    done with maps

--- SOLUTIONS/JAVA/HashMapInJava.java
+++ SOLUTIONS/JAVA/HashMapInJava.java
@@ -45,3 +45,41 @@ public class HashMapInJava {
     }
  
 }
+
+Java HashMap example to replace() elements
+Here, we see different ways to replace elements.
+
+import java.util.*;  
+class HashMap3{  
+ public static void main(String args[]){  
+   HashMap<Integer,String> hm=new HashMap<Integer,String>();    
+      hm.put(100,"Amit");    
+      hm.put(101,"Vijay");    
+      hm.put(102,"Rahul");   
+      System.out.println("Initial list of elements:");  
+     for(Map.Entry m:hm.entrySet())  
+     {  
+        System.out.println(m.getKey()+" "+m.getValue());   
+     }  
+     System.out.println("Updated list of elements:");  
+     hm.replace(102, "Gaurav");  
+     for(Map.Entry m:hm.entrySet())  
+     {  
+        System.out.println(m.getKey()+" "+m.getValue());   
+     }  
+     System.out.println("Updated list of elements:");  
+     hm.replace(101, "Vijay", "Ravi");  
+     for(Map.Entry m:hm.entrySet())  
+     {  
+        System.out.println(m.getKey()+" "+m.getValue());   
+     }   
+     System.out.println("Updated list of elements:");  
+     hm.replaceAll((k,v) -> "Ajay");  
+     for(Map.Entry m:hm.entrySet())  
+     {  
+        System.out.println(m.getKey()+" "+m.getValue());   
+     }  
+ }  
+}  
+
+
